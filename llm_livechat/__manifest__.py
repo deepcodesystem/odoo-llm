6b33c1dfc1b2d0152ebbb9c74cca526c31652eb0
DeepCode <81932927+deepcodesystem@users.noreply.github.com>
2026-04-08 13:15:22 +0100

    Comment out ir.model.access.csv in manifest
    Comment out the access control CSV file in manifest.

--- llm_livechat/__manifest__.py
+++ llm_livechat/__manifest__.py
@@ -24,7 +24,7 @@ Features:
         "im_livechat",
     ],
     "data": [
-        "security/ir.model.access.csv",
+        #"security/ir.model.access.csv",
         "data/res_users.xml",
         "views/im_livechat_channel_views.xml",
         "data/ir_cron.xml",
